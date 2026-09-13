b1f25891fe21d6855ac15f903e8bb461f9abbb37
Papinio89 <128943843+Papinio89@users.noreply.github.com>
2026-09-13 09:51:01 +0200

    Update update_news.py

--- update_news.py
+++ update_news.py
@@ -104,7 +104,7 @@ Dane wejściowe:
 items = []
 try:
     response = client.models.generate_content(
-        model='gemini-3.6-pro',
+        gemini-3.1-pro-preview',
         contents=prompt,
     )
     text_res = response.text.strip()
